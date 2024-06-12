e3de225f8d2480d3d969e9ba214477dbaec61c43
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-06-12 15:57:18 +0530

    component-valid

--- src/mushrooms/components/data_validation.py
+++ src/mushrooms/components/data_validation.py
@@ -1,7 +1,7 @@
 from mushrooms.entity import DataValidationConfig
 import pandas as pd
 
-class DataValiadtion:
+class DataValidation:
     def __init__(self, config: DataValidationConfig):
         self.config = config
 
