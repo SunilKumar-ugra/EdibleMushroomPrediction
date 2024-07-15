177682f3d549e3c9f07548e8b40b691ff3922509
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-15 19:53:14 +0530

    Valid-Pipeline

--- src/mushrooms/pipeline/stage_02_data_validation.py
+++ src/mushrooms/pipeline/stage_02_data_validation.py
@@ -1,5 +1,6 @@
 from mushrooms.config import ConfigurationManager
 from mushrooms.components.data_validation import DataValidation
+from mushrooms import logger
 
 class DataValidationTrainingPipeline:
     def __init__(self):
@@ -9,3 +10,15 @@ class DataValidationTrainingPipeline:
         data_validation_config = config.get_data_validation_config()
         data_validation = DataValidation(config=data_validation_config)
         data_validation.validate_all_columns()
+
+if __name__ == '__main__':
+    try:
+        STAGE_NAME = "Data Validation stage"
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_validation = DataValidationTrainingPipeline()
+        data_validation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
