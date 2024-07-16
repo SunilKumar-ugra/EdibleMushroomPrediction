cadd93bc3d9adc94387bb6d0ad8970fd4c340cc2
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-16 19:50:33 +0530

    Main-Transform

--- main.py
+++ main.py
@@ -24,3 +24,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+from mushrooms.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
