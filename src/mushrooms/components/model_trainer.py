643ce2402dcc87b848537b019c2e6aa68c99c2ac
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-17 14:09:33 +0530

    Component-Train

---
+++ src/mushrooms/components/model_trainer.py
@@ -0,0 +1,27 @@
+from mushrooms.entity import ModelTrainerConfig
+import pandas as pd
+import os
+from mushrooms import logger
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+
+    
+    def train(self):
+        train_data = pd.read_csv(self.config.train_data_path)
+        test_data = pd.read_csv(self.config.test_data_path)
+
+
+        train_x = train_data.drop([self.config.target_column], axis=1)
+        test_x = test_data.drop([self.config.target_column], axis=1)
+        train_y = train_data[[self.config.target_column]]
+        test_y = test_data[[self.config.target_column]]
+
+        clf = RandomForestClassifier(n_estimators = 100) 
+        clf.fit(train_x, train_y)
+
+        joblib.dump(clf, os.path.join(self.config.root_dir, self.config.model_name))
+
