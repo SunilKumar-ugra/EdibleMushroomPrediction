1d7311c374b19387ddcd28da29dc76b1115e6e13
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-20 19:22:38 +0530

    CFG-Evaluation

--- src/mushrooms/config/__init__.py
+++ src/mushrooms/config/__init__.py
@@ -3,7 +3,8 @@ from mushrooms.utils import read_yaml, create_directories
 from mushrooms.entity import (DataIngestionConfig,
                               DataValidationConfig,
                               DataTransformationConfig,
-                              ModelTrainerConfig)
+                              ModelTrainerConfig,
+                              ModelEvaluationConfig)
 
 
 class ConfigurationManager:
@@ -81,3 +82,24 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri=config.mlflow_uri,
+           
+        )
+
+        return model_evaluation_config
